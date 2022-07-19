3baee1511f8c7035726ffd136f7948636f86b5e9
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-07-19 03:53:34 -0400

    🛸

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ColorModeScript } from "@chakra-ui/react";
+import theme from "./theme";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
