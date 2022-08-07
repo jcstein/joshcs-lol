f08cf13b5c0d167a7a53811ea86352f6299afac6
joshcs.eth <46639943+jcstein@users.noreply.github.com>
2022-08-07 10:23:28 -0400

    tree

--- src/tree.tsx
+++ src/tree.tsx
@@ -1,113 +1,15 @@
-// import { Avatar, VStack, Link, Divider, Text, Button } from "@chakra-ui/react";
-// import { Buttons } from "./Components/buttons";
-// import { About } from "./Components/about";
-// import {
-//   FaInstagram,
-//   FaTwitter,
-//   FaGlobe,
-//   FaGithub,
-//   FaLinkedin,
-// } from "react-icons/fa";
+// import { ChakraProvider } from "@chakra-ui/react";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import Links from "./Links";
+// import { ColorModeScript } from "@chakra-ui/react";
+// import theme from "./theme";
 
-// function App() {
-//   return (
-//     <div>
-//       <Buttons />
-//       <VStack p="7">
-//         <About />
-//         <Divider />
-//         <VStack spacing={4} direction="column" align="center" width="90%">
-//           <Button
-//             colorScheme="purple"
-//             size="lg"
-//             rightIcon={<FaTwitter />}
-//             onClick={() =>
-//               window.open("https://twitter.com/JoshCStein", "_blank")
-//             }
-//             width="100%"
-//             _hover={{ transform: "scale(1.1)" }}
-//             rounded="full"
-//           >
-//             Twitter
-//           </Button>
-//           <Button
-//             colorScheme="purple"
-//             size="lg"
-//             rightIcon={<FaGlobe />}
-//             onClick={() => window.open("https://joshcs.lol", "_blank")}
-//             width="100%"
-//             _hover={{ transform: "scale(1.1)" }}
-//             rounded="full"
-//           >
-//             Portfolio
-//           </Button>
-//           <Button
-//             colorScheme="purple"
-//             size="lg"
-//             rightIcon={<FaGithub />}
-//             onClick={() => window.open("https://github.com/jcstein", "_blank")}
-//             width="100%"
-//             _hover={{ transform: "scale(1.1)" }}
-//             rounded="full"
-//           >
-//             GitHub
-//           </Button>
-//           <Button
-//             colorScheme="purple"
-//             size="lg"
-//             rightIcon={<FaLinkedin />}
-//             onClick={() =>
-//               window.open("https://linkedin.com/in/joshcstein", "_blank")
-//             }
-//             width="100%"
-//             _hover={{ transform: "scale(1.1)" }}
-//             rounded="full"
-//           >
-//             LinkedIn
-//           </Button>
-//           <Button
-//             colorScheme="purple"
-//             size="lg"
-//             rightIcon={<FaInstagram />}
-//             onClick={() =>
-//               window.open("https://www.instagram.com/joshcstein/", "_blank")
-//             }
-//             width="100%"
-//             _hover={{ transform: "scale(1.1)" }}
-//             rounded="full"
-//           >
-//             Instagram
-//           </Button>
-//           <Button
-//             colorScheme="purple"
-//             size="lg"
-//             rightIcon={<FaGithub />}
-//             onClick={() =>
-//               window.open(
-//                 "https://github.com/jcstein/vite-tree/generate",
-//                 "_blank"
-//               )
-//             }
-//             width="100%"
-//             _hover={{ transform: "scale(1.1)" }}
-//             rounded="full"
-//           >
-//             Use this Template
-//           </Button>
-//         </VStack>
-//         <Divider />
-//         <Link href="/" pt="8">
-//           <Avatar src="/jcs.png" mx="auto" size="md" />
-//         </Link>
-//         <Text>
-//           This website is{" "}
-//           <Link href="https://github.com/jcstein/joshcs-lol" isExternal>
-//             open-source
-//           </Link>
-//         </Text>
-//       </VStack>
-//     </div>
-//   );
-// }
-
-// export default App;
+// ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+//   <React.StrictMode>
+//     <ChakraProvider theme={theme}>
+//       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+//       <Links />
+//     </ChakraProvider>
+//   </React.StrictMode>
+// );
